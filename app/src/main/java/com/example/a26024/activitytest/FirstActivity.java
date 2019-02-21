4a3db4b13d05579946aefea61f25bbf7eb405416
wukong <2602401180@qq.com>
2019-02-21 11:37:51 +0800

    create an activity with button and menu

---
+++ app/src/main/java/com/example/a26024/activitytest/FirstActivity.java
@@ -0,0 +1,46 @@
+package com.example.a26024.activitytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this,"haha",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.add_item:
+                Toast.makeText(this,"you clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+               finish();
+               break;
+               default:
+        }
+        return true;
+    }
+}
